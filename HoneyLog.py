55f3fca8e91c00318b928533ac8f9d6f04a2b898
Mark Riffey <mriffey@rescuemarketing.com>
2020-10-12 00:41:14 -0600

    initial commit

---
+++ HoneyLog.py
@@ -0,0 +1,29 @@
+import libhoney
+import argparse
+import pendulum
+import json
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--apikey', type=str)
+parser.add_argument('--dataset', type=str)
+parser.add_argument('--logfile', type=str)
+parser.add_argument('--timestamp', type=str)
+
+args = parser.parse_args()
+libhoney.init(writekey=args.apikey, dataset=args.dataset, debug=True)
+honeyevent = libhoney.new_event()
+
+with open(args.logfile) as json_file:
+    logdata = json.load(json_file)
+    print(logdata)
+    for p in logdata['logs']:
+        print('timestamp: ' + p['created_at'])
+        print('log: ' + p['log'])
+        print('')
+        dt = pendulum.parse(p['created_at'])
+        honeyevent.created_at = dt
+        honeyevent.add_field('log', p['log'])
+        honeyevent.send()
+
+libhoney.close()
